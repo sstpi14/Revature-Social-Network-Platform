4e26ec08b17d7a8a6a320aae0ad6ac6e29ade181
freelancer9977 <masp3836@colorado.edu>
2022-03-30 14:13:51 -0600

    adding base line backend

---
+++ RevatureSocialNetwork/src/main/java/models/Display.java
@@ -0,0 +1,23 @@
+package models;
+
+import lombok.*;
+
+import java.sql.Blob;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Display {
+    private Integer id;
+    private Blob img;
+    private Integer profileId;
+    private Boolean liked;
+    private Integer numberOfLieckes;
+    private String description;
+
+    public Display(Integer profileId) {
+        this.profileId = profileId;
+    }
+}
