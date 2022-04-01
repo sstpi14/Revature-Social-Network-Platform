8b3c52c9e6eab3a82eb05ff00fe3d2e06ad1ca08
freelancer9977 <masp3836@colorado.edu>
2022-04-01 15:05:13 -0600

    added basic fuctions to profile controller

--- EnergySocialNetwork/src/main/java/com/revature/EnergySocialNetwork/controllers/ProfileController.java
+++ EnergySocialNetwork/src/main/java/com/revature/EnergySocialNetwork/controllers/ProfileController.java
@@ -1,4 +1,40 @@
 package com.revature.EnergySocialNetwork.controllers;
 
+import com.revature.EnergySocialNetwork.models.Profile;
+import com.revature.EnergySocialNetwork.services.ProfileService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("profile")
 public class ProfileController {
+    @Autowired
+    private ProfileService profileService;
+
+    @GetMapping("Login/{profileId}")
+    public Profile getOne(@PathVariable Integer profileId){
+        return profileService.getOne(profileId);
+    }
+
+    @PostMapping("Register")
+    public Profile createPorfile(@RequestBody Profile profile){
+        return profileService.createOne(profile);
+    }
+
+    @PutMapping
+    public Profile updateProfile(@RequestBody Profile profile){
+        return profileService.updateOne(profile);
+    }
+
+    @GetMapping("{username}")
+    public Profile getOneProfileByUsername(@PathVariable String username){
+        return profileService.getOneGivenUsername(username);
+    }
+
+    @GetMapping
+    public List<Profile> getAllProfile(){
+        return profileService.getAll();
+    }
 }
