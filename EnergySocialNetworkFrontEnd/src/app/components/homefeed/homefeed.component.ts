7208e0b8c7681c57c678fd19a052f29bd6090ade
freelancer9977 <masp3836@colorado.edu>
2022-04-19 13:06:54 -0600

    display all displays

--- EnergySocialNetworkFrontEnd/src/app/components/homefeed/homefeed.component.ts
+++ EnergySocialNetworkFrontEnd/src/app/components/homefeed/homefeed.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { display } from 'src/app/models/display';
+import { Profile } from 'src/app/models/profile';
 import { DisplayServiceService } from 'src/app/service/display-service.service';
 
 @Component({
@@ -14,21 +16,26 @@ export class HomefeedComponent implements OnInit {
   user2:display={username:"kev",desciption:"bye",img:"",like:true};
   //displays:Array<display> = [ this.user,this.user2 ];
   displays:Array<any> = [];
+  profile:Array<Profile> = [];
   isLiked:boolean = false;
   isVisable:boolean = false;
-  constructor(private dispaySer : DisplayServiceService) {}
+  constructor(private dispaySer : DisplayServiceService,private router : Router) {}
 
   ngOnInit(): void {
     this.dispaySer.getAllDisplays().subscribe(responseBody =>{
-      this.displays = responseBody.data;
+      this.displays = responseBody;
+      this.profile = responseBody[0].profile;
+      console.log(this.displays);
     })
   }
 
   goToUser(e:any){
+    this.router.navigate(["/user"]);
     console.log(e.target.innerText);
   }
 
   goToOtherUser(e:any){
+    this.router.navigate(["/otheruser"]);
     console.log(e.target.innerText);
   }
 
--- EnergySocialNetworkFrontEnd/src/app/service/display-service.service.ts
+++ EnergySocialNetworkFrontEnd/src/app/service/display-service.service.ts
@@ -1,5 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { JsonResponse } from '../models/jsonresponse';
 
 @Injectable({
   providedIn: 'root'
@@ -8,9 +10,19 @@ export class DisplayServiceService {
 
   constructor(private httpCli:HttpClient) { }
 
+  httpOptions = {
+    headers: new HttpHeaders({
+      "Content-Type":"application/json"
+    })
+  }
+
   getAllDisplays(){
     return this.httpCli.get<any>("http://localhost:9000/display",{
       withCredentials: true
     });
+  }
+
+  createDisplay(display:any): Observable<JsonResponse>{
+    return this.httpCli.post<JsonResponse>("http://localhost:9000/display", JSON.stringify(display), this.httpOptions);
   }
 }
